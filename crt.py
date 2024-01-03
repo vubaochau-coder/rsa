03249011fb92a881c95c4ef2e8696e66517a4c95
vubaochau-coder <hirusakunasaga@gmail.com>
2024-01-03 14:16:40 +0700

    save

--- crt.py
+++ crt.py
@@ -36,30 +36,30 @@ def crt_rsa(c, d, prime1, prime2):  # m = c^d mod (prime1 * prime2)
     red_private1 = PowMod(prime1, 1, prime2) #prime1 % prime2
     red_res1 = PowMod(res1, 1, prime2) #res1 % prime2
     # print(f'Function: {red_private1}x + {red_res1} = {res2} mod {prime2}')
-    # print(f'Function: {red_private1}x = {res2 - red_res1} mod {prime2}')
+    print(f'Function: {red_private1}x = {res2 - red_res1} mod {prime2}')
     
     invert = InvertModulo(red_private1, prime2)
     x = PowMod(invert * (res2 - red_res1), 1, prime2)
     # x = solve_equation(red_private1, res2 - red_res1, prime2)
-    print(x)
+    # print(x)
     
     result = prime1 * x + res1
-    print(result)
+    # print(result)
     return result
 
-def find_inverse(a, m):
-    for x in range(1, m):
-        if (a * x) % m == 1:
-            return x
-    return None
+# def find_inverse(a, m):
+#     for x in range(1, m):
+#         if (a * x) % m == 1:
+#             return x
+#     return None
 
-def solve_equation(a, b, m):
-    inverse_a = find_inverse(a, m)
-    if inverse_a is None:
-        return "No inverse exists."
+# def solve_equation(a, b, m):
+#     inverse_a = find_inverse(a, m)
+#     if inverse_a is None:
+#         return "No inverse exists."
 
-    x = (inverse_a * b) % m
-    return x
+#     x = (inverse_a * b) % m
+#     return x
 
 if __name__ == '__main__':
     # print('InvertModulo(60, 17)')
