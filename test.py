6a64e5b1e0fe4fe080e376b2b6fcc9d9fbd74257
Khanh20521455 <20521455@gm.uit.edu.vn>
2024-01-06 06:39:49 +0700

    test case

--- test.py
+++ test.py
@@ -1,64 +1,3 @@
-import unittest
-from pow_mod import PowMod
-from prime_generate import prime_generate 
-from get_key import public_key_1, public_key_2, private_key
-from crt_rsa_decrypt import decrypt_crt 
-from rsa_encrypt import encrypt
-from rsa_decrypt import decrypt
-import time
-
-class TestRSA(unittest.TestCase):
-
-    def test_encryption_decryption(self):
-        bit_length = 32  # Choose an appropriate bit length for testing
-        msg = "Hello, World!"  # Test message
-
-        p = prime_generate(bit_length)
-        q = prime_generate(bit_length)
-        m = (p - 1) * (q - 1)
-        n = public_key_1(p, q)
-        e = public_key_2(p, q)
-        d = private_key(m, e)
-
-        # encryption
-        C = encrypt(msg, e, n)
-
-        # decryption with RSA
-        decrypted_rsa = decrypt(C, d, n)
-        self.assertEqual(decrypted_rsa, msg)
-
-        # decryption with CRT-RSA
-        decrypted_crt = decrypt_crt(C, d, p, q)
-        self.assertEqual(decrypted_crt, msg)
-
-    def test_performance(self):
-        bit_length = 32  # Choose an appropriate bit length for testing
-        msg = "Hello, World!"  # Test message
-
-        p = prime_generate(bit_length)
-        q = prime_generate(bit_length)
-        m = (p - 1) * (q - 1)
-        n = public_key_1(p, q)
-        e = public_key_2(p, q)
-        d = private_key(m, e)
-
-        # encryption
-        C = encrypt(msg, e, n)
-
-        # measure decryption time with RSA
-        start_time_decrypt_rsa = time.time()
-        decrypt(C, d, n)
-        time_rsa = time.time() - start_time_decrypt_rsa
-
-        # measure decryption time with CRT-RSA
-        start_time_decrypt_crt = time.time()
-        decrypt_crt(C, d, p, q)
-        time_crt = time.time() - start_time_decrypt_crt
-
-        self.assertLess(time_crt, time_rsa)  # CRT-RSA should be faster
-
-if __name__ == "__main__":
-    unittest.main()
 
 import unittest
 from pow_mod import PowMod
@@ -71,10 +10,7 @@ import time
 
 class TestRSA(unittest.TestCase):
 
-    def test_encryption_decryption(self):
-        bit_length = 32  # Choose an appropriate bit length for testing
-        msg = "Hello, World!"  # Test message
-
+    def run_test_case(self, bit_length, msg):
         p = prime_generate(bit_length)
         q = prime_generate(bit_length)
         m = (p - 1) * (q - 1)
@@ -86,38 +22,31 @@ class TestRSA(unittest.TestCase):
         C = encrypt(msg, e, n)
 
         # decryption with RSA
+        start_time_decrypt_rsa = time.time()
         decrypted_rsa = decrypt(C, d, n)
+        time_rsa = time.time() - start_time_decrypt_rsa
         self.assertEqual(decrypted_rsa, msg)
+        
 
         # decryption with CRT-RSA
+        start_time_decrypt_crt = time.time()
         decrypted_crt = decrypt_crt(C, d, p, q)
+        time_crt = time.time() - start_time_decrypt_crt
         self.assertEqual(decrypted_crt, msg)
 
-    def test_performance(self):
-        bit_length = 32  # Choose an appropriate bit length for testing
-        msg = "Hello, World!"  # Test message
+        self.assertLess(time_crt, time_rsa) 
 
-        p = prime_generate(bit_length)
-        q = prime_generate(bit_length)
-        m = (p - 1) * (q - 1)
-        n = public_key_1(p, q)
-        e = public_key_2(p, q)
-        d = private_key(m, e)
 
-        # encryption
-        C = encrypt(msg, e, n)
+    def test_encryption_decryption_custom(self):
+        # List of custom test cases (bit_length, message)
+        test_cases = [(256, "Hello, World!"), (256, "Seminar các vấn đề hiện đại của CNPM"), (256, "1234567890-=!@#$%^&*()_+{}[]|\":>?<,./"),
+                      (512, "Hello, World!"), (512, "Seminar các vấn đề hiện đại của CNPM"),(512, "1234567890-=!@#$%^&*()_+{}[]|\":>?<,./")]
 
-        # measure decryption time with RSA
-        start_time_decrypt_rsa = time.time()
-        decrypt(C, d, n)
-        time_rsa = time.time() - start_time_decrypt_rsa
+        # Run test for each custom test case
+        for bit_length, msg in test_cases:
+            with self.subTest(bit_length=bit_length, msg=msg):
+                self.run_test_case(bit_length, msg)
 
-        # measure decryption time with CRT-RSA
-        start_time_decrypt_crt = time.time()
-        decrypt_crt(C, d, p, q)
-        time_crt = time.time() - start_time_decrypt_crt
-
-        self.assertLess(time_crt, time_rsa)  # CRT-RSA should be faster
 
 if __name__ == "__main__":
     unittest.main()
